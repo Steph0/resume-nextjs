75707fa697d487b8d14982f7ca1e8fb4d337ff1d
Steph0 <Steph0@users.noreply.github.com>
2025-03-08 01:06:21 +0100

    feat: add image profile

--- __tests__/src/app/[locale]/page.test.tsx
+++ __tests__/src/app/[locale]/page.test.tsx
@@ -1,8 +1,8 @@
 import { describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { AbstractIntlMessages, NextIntlClientProvider } from 'next-intl';
 import messages from '@/messages/en.json';
-import { DashboardPage } from '@/src/app/[locale]/page';
+import DashboardPage from '@/src/app/[locale]/page';
 
 describe('DashboardPage', () => {
   test('it should contain a presentation', async () => {
@@ -25,5 +25,25 @@ describe('DashboardPage', () => {
       name: "Hi! I'm Stephen",
     });
     expect(link).toBeDefined();
+  });
+
+  test('it should contain my profile picture', async () => {
+    // given
+    const locale = 'en';
+
+    // when
+    render(
+      <NextIntlClientProvider
+        locale={locale}
+        messages={messages as AbstractIntlMessages}
+      >
+        <DashboardPage />
+      </NextIntlClientProvider>,
+    );
+
+    // then
+    const cards = screen.getAllByRole('article');
+    const image = within(cards[0]).getByAltText('Photo of the author');
+    expect(image).toBeDefined();
   });
 });
